ab1c128481c5dc05bd0cf7b59ca7ae3c07d14893
Martese Goosby <mgoosby@emich.edu>
2021-09-05 20:04:32 -0400

    Created and exported function getIsoCode

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import * as isoCodes from './jsons/codes.json'
-import * as salesTaxRates from './jsons/sales-tax-rates.json'
+const isoCodes = require('./jsons/codes.json');
+const salesTaxRates = require('./jsons/sales-tax-rates.json');
 
-export default function useSalesTaxRates({isoCountryCode,region}){
+const getIsoCode=({isoCountryCode,region})=>{
+	let regionCode = region
+	// if region isnt isoCode convert it to iso
+	if (regionCode.length > 3 || !isoCodes[regionCode]){
+		regionCode = regionCode.normalize("NFD").replace(/[\u0300-\u036f]/g,"").toLowerCase()
+		let isoCodeInfo = isoCodes[regionCode]
+		if(!isoCodeInfo)
+			return null
+		else{
+			regionCode = isoCodeInfo.provinceCode
+			if(isoCountryCode != isoCodeInfo.countryCode)
+				isoCountryCode = isoCodeInfo.countryCode
+			return { isoCountryCode, isoRegionCode:regionCode }
+		}
+	}
+}
+const useSalesTaxRates=({isoCountryCode,region})=>{
 	let regionTaxRate = 0.0
 	if(!isoCountryCode && !region)
 		return regionTaxRate
-	// if region isnt isoCode convert it to iso
-	if (region.length > 3 || !isoCodes[region]){
-		region = region.normalize("NFD").replace(/[\u0300-\u036f]/g,"").toLowerCase()
-		let isoRegionCode = isoCodes[region]
-		if(!isoRegionCode)
-			return regionTaxRate
-		else{
-			isoRegionCode = isoRegionCode.provinceCode
-			if(!isoCountryCode)
-				isoCountryCode = isoRegionCode.countryCode
-		}
-		if ( isoRegionCode )
-			region = isoRegionCode
-	}
-	console.log(isoCountryCode, region )
-	let countryTaxInfo = salesTaxRates[isoCountryCode]
-	let countryTaxRate = countryTaxInfo.rate
+
+	let isoCode = getIsoCode({isoCountryCode,region})
+	if(!isoCode)
+		return regionTaxRate
+	let countryTaxInfo = salesTaxRates[isoCode.isoCountryCode]
+	let countryTaxRate = countryTaxInfo.rate || 0.0
 	if(countryTaxInfo.states)
-		regionTaxRate = countryTaxInfo.states[region].rate
+		regionTaxRate = countryTaxInfo.states[isoCode.isoRegionCode].rate
 	return regionTaxRate + countryTaxRate
 }
+exports = {getIsoCode,useSalesTaxRates}
